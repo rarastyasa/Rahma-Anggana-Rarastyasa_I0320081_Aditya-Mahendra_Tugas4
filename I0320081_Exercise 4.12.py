b68d891c27e4323dbcceff275b6ff492262e8434
Rahma Anggana Rarastyasa <rahmaanggana04@gmail.com>
2021-03-23 21:26:31 +0700

    Praktek Programa Komputer Tugas 4

---
+++ I0320081_Exercise 4.12.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+a = 69              # 60 = 0011 1100
+b = 13              # 13 = 0000 1101
+c = 0
+
+c = a & b;          # 12 = 0000 1100
+print ("Line 1 - Value of c is ", c)
+
+c = a | b;          # 61 = 0011 1101
+print ("Line 2 - Value of c is ", c)
+
+c = a ^ b;          # 49 = 0011 0001
---
+++ I0320081_Exercise 4.8.py
@@ -0,0 +1,8 @@
+# strings
+str = "Jane Doe"
+
+# character
+ch = str[1]
+
+# output
+print(ch)       # a
