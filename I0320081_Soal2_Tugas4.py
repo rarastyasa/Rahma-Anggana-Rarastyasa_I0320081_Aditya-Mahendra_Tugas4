226f6e7b14254e9d0749ee1c0e23ffa96ea115c6
Rahma Anggana Rarastyasa <rahmaanggana04@gmail.com>
2021-03-27 01:14:23 +0700

    Praktek Programa Komputer Tugas 4

---
+++ I0320081_Soal2_Tugas4.py
@@ -0,0 +1,8 @@
+#Import math library
+import math
+
+# Round numbers down to the nearest integer
+p=int(input("Masukkan Angka Pertama :"))
+q=int(input("Masukkan Angka Kedua :"))
+r=p/q
+print(math.floor(r))
---
+++ I0320081_Soal3_Tugas4.py
@@ -0,0 +1,4 @@
+batas_berat = 50
+barang =int(input("Masukkan Berat Bagasi :"))
+
+print(batas_berat < barang)
